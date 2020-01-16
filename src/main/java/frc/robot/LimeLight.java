b958521f269a17705591ff05d70d02c80582bc05
Branden Amstutz <brandenamstutz@gmail.com>
2020-01-16 08:54:54 -0500

    Working on constructor for limeLight

--- src/main/java/frc/robot/LimeLight.java
+++ src/main/java/frc/robot/LimeLight.java
@@ -7,16 +7,38 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class LimeLight{
 
     public NetworkTable table;
-    public NetworkTableEntry tx, ty, ta, tv, ledMode;
+    public NetworkTableEntry tx, ty, ta, tv, ts, tl, tshort, tlong, thor, tvert, getpipe, camtran, ledMode;
 
     //constructor to create the limelight and its values
     //////Branden Amstutz
     public LimeLight() {
+
         table = NetworkTableInstance.getDefault().getTable("limelight");
+        // horizontal offset of cross hair to target
         tx = table.getEntry("tx");
+        // vertical offset of cross hair to target
         ty = table.getEntry("ty");
+        //target area: 0% of image to 100%
         ta = table.getEntry("ta");
+        // valid target: 0 - invalid, 1 - valid
         tv = table.getEntry("tv");
+        // Skew or rotation
+        ts = table.getEntry("ts");
+        //pipeline Latency
+        tl = table.getEntry("tl");
+        //sidelength of the shortest side of fitted bounding box
+        tshort = table.getEntry("tshort");
+        //sidelength of the longest side of fitted bounding box
+        tlong = table.getEntry("tlong");
+        //horizontal sidelength of rough bounding box
+        thor = table.getEntry("thor");
+        //vertical sidelength of rough bounding box
+        tvert = table.getEntry("tvert");
+        // true active pipeline index of the camera
+        getpipe = table.getEntry("getpipe");
+        // Results of a 3D position solution, 6 numbers: Translation (x,y,y) Rotation(pitch,yaw,roll)
+        camtran = table.getEntry("camtran");
+        //state of LEDs: 1.0 - on, 2.0 - blink, 3.0 - off
         ledMode = table.getEntry("ledMode");
         
     }
