e7cd4a09a3899e6b2df90c870dc444b1bea9cfc6
Carter Harris <carter.50@hotmail.com>
2020-01-28 13:47:25 -0500

    Commented ArcadeDrive

--- src/main/java/frc/controller/controlSchemes/ArcadeDrive.java
+++ src/main/java/frc/controller/controlSchemes/ArcadeDrive.java
@@ -18,12 +18,18 @@ import edu.wpi.first.wpilibj.smartdashboard.*;
  */
 public class ArcadeDrive extends ControlScheme {
 
+    //Create all objects & a speedMode object
     XboxController driveController, armController;
 
     SpeedMode speedMode;
 
     boolean lowGear;
 
+    /**
+     * 
+     * @param driveControllerPort Controller port the drive controller is connected to, probably 0
+     * @param armControllerPort Controller port the arm controller is connect to, problably 1
+     */
     public ArcadeDrive(int driveControllerPort, int armControllerPort) {
         driveController = new XboxController(driveControllerPort);
         armController = new XboxController(armControllerPort);
@@ -38,6 +44,7 @@ public class ArcadeDrive extends ControlScheme {
      * 
      */
     public void drive(SingDrive drive, DrivePneumatics pneumatics) {
+        //Switch speed mode object, set to low with left bumber and high with right bumper
         if(driveController.getLB()) {
             speedMode = SpeedMode.SLOW;
         }
@@ -45,10 +52,12 @@ public class ArcadeDrive extends ControlScheme {
         else if(driveController.getRB()) {
             speedMode = SpeedMode.FAST;
         }
+        //Put current speedMode on SmartDashboard
         SmartDashboard.putString("Speed Mode", "" + speedMode);
 
 
-
+        //Change physical pneumatic gearing with the start button (high gear) and back button (low gear).
+        //This sets a boolean lowGear
         if(driveController.getStartButton()) {
             lowGear = false;
         }
@@ -57,6 +66,7 @@ public class ArcadeDrive extends ControlScheme {
             lowGear = true;
         }
 
+        // lowGear is used to actually set the drive pneumatics to intended value.
         if(lowGear) {
             pneumatics.setLow();
         }
@@ -65,11 +75,20 @@ public class ArcadeDrive extends ControlScheme {
             pneumatics.setHigh();
         }
 
+        //
+        //IMPORTANT
+        //
+        //The only line actually needed to drive - takes in control sticks, speed mode, and drives based on BasicDrive.
         drive.arcadeDrive(driveController.getLS_Y(), driveController.getRS_X(), 0.0, true, speedMode);
 
     }
     
+    /**
+     * Controls flywheel - turns only on (forward) and off
+     * @param flywheel Takes in flywheel object to control.
+     */
     public void Flywheel(Flywheel flywheel) {
+        // When A is pressed and held, turn on the flywheel to the constant speed defined in the class.
         if(armController.getAButton()) {
             flywheel.flywheelForward();
         }
@@ -79,8 +98,12 @@ public class ArcadeDrive extends ControlScheme {
         }
     }
 
-
+    /**
+     * Only turns on the painfully bright Limelight LEDs when they're being used
+     * @param limelight takes in Limelight object
+     */
     public void ledMode(LimeLight limeLight ){
+
         /*
         if(driveController.getXButton() || driveController.getYButton()){
             limeLight.ledOff(limeLight);
