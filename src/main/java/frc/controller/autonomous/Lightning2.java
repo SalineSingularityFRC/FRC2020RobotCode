92d5b0407cfc09cb6a48821302c0bfa9020a51a5
John Chikwe <Johnchikwe2@gmail.com>
2020-02-14 21:14:12 -0500

    made 2 classes and adjusted to the field with new estimated angles

---
+++ src/main/java/frc/controller/autonomous/Lightning2.java
@@ -0,0 +1,21 @@
+package frc.controller.autonomous;
+
+import frc.singularityDrive.SingDrive;
+import frc.robot.LimeLight;
+
+public class Lightning2 extends AutonControlScheme{
+
+    public Lightning2(SingDrive drive, LimeLight limeLight){
+    super(drive, limeLight);//references constructor of super class
+}
+    public void moveAuton(){
+        super.vertical(60, 0.5);
+        super.rotate(90, false);
+        super.vertical(58, 0.5);
+        super.rotate(90, true);
+        super.vertical(40, 0.5);
+        super.adjustToTarget();
+        super.vertical(30, 0.5);
+        super.shoot();
+    }
+}
