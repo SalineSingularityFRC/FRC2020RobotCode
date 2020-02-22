7ad5002415e4cf168b28ed4d2cd575ea8ee15807
Jared Webb <89JBWebb@gmail.com>
2020-02-21 21:25:53 -0500

    added shooter function

--- src/main/java/frc/controller/autonomous/Lightning2.java
+++ src/main/java/frc/controller/autonomous/Lightning2.java
@@ -2,11 +2,13 @@ package frc.controller.autonomous;
 
 import frc.singularityDrive.SingDrive;
 import frc.robot.LimeLight;
+import frc.robot.Flywheel;
+import frc.robot.Conveyor;
 
 public class Lightning2 extends AutonControlScheme {
 
-    public Lightning2(SingDrive drive, LimeLight limeLight) {
-        super(drive, limeLight);
+    public Lightning2(SingDrive drive, LimeLight limeLight, Flywheel flywheel, Conveyor conveyor) {
+        super(drive, limeLight, flywheel, conveyor);
     }
 
     public void moveAuton() {
